5681bf708d1280248bb8dc263e27e816912d8608
snehamoybag <102692389+snehamoybag@users.noreply.github.com>
2025-10-08 20:09:57 +0530

    fix: stop passport from sending plain text as response
    Stop passport from sending plain text as response when authorization
    fails.

--- middlewares/auth/verify-auth-token.middleware.ts
+++ middlewares/auth/verify-auth-token.middleware.ts
@@ -1,8 +1,29 @@
 import { RequestHandler } from "express";
 import passport from "../../configs/passport.config";
+import { AuthenticateCallback } from "passport";
+import { ErrorUnauthorized } from "../../libs/http-exceptions";
 
 const verifyAuthToken: RequestHandler = (req, res, next) => {
-  return passport.authenticate("jwt", { session: false })(req, res, next);
+  const authenticateCallback: AuthenticateCallback = (err, user) => {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    if (!user) {
+      next(new ErrorUnauthorized("Invalid auth token."));
+      return;
+    }
+
+    req.user = user;
+
+    next();
+  };
+  return passport.authenticate("jwt", { session: false }, authenticateCallback)(
+    req,
+    res,
+    next,
+  );
 };
 
 export default verifyAuthToken;
