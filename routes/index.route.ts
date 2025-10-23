cac169d8c3d95a79adf66756a2d397b934e26567
snehamoybag <102692389+snehamoybag@users.noreply.github.com>
2025-10-23 20:30:36 +0530

    refractor: add a POST method for index route
    this route can be used to verify if the issued JWT is still valid.

--- routes/index.route.ts
+++ routes/index.route.ts
@@ -1,9 +1,20 @@
 import { Router } from "express";
 import { SuccessResponse } from "../libs/http-response-shapes";
+import verifyAuthToken from "../middlewares/auth/verify-auth-token.middleware";
 
 const index = Router();
 
 index.get("/", (_req, res) => {
+  res.json(
+    new SuccessResponse("Hi from Snehamoy's blog.", {
+      name: "Snehamoy's Blog",
+      version: "1.0.0",
+    }),
+  );
+});
+
+// use this route to verify if the issued JWT is still valid
+index.post("/", verifyAuthToken, (_req, res) => {
   res.json(
     new SuccessResponse("Hi from Snehamoy's blog.", {
       name: "Snehamoy's Blog",
